2909fef5476ab4b519bb9ca7282c58a1c4e41f64
sunchao <sunchao1@7net.cc>
2021-02-26 18:13:59 +0800

    bug

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const version = require('./package').version;                 // 版本号
 /* = package import
 -------------------------------------------------------------- */
 const program = require('commander');                         // 命令行解析
-
 /* = task events
 -------------------------------------------------------------- */
 const createProject = require('./lib/create-project'); // 创建项目
 const createPage = require('./lib/create-page'); // 创建页面
+const generate = require('./lib/generate'); // 创建
 const createComponent = require('./lib/create-component'); // 创建组件
                                            
 /* = set version
@@ -30,17 +30,18 @@ program
 
 // 快速生成模版 页面/组件
 program
-  .command("g <type> <name>")
+  .command("generate <type> <name>")
+  .alias('g')
   .description("创建页面或组件",{
     type:'值为page(页面)或com(组件)',
     name:'页面或组件的名称'
   })
-  .option("-d,--dir <dir>", "指定创建的目录，不指定时则在项目src/views目录下创建页面或src/components目录下创建组件（执行命令目录路径下有这两个目录时），或者执行命令目录路径下创建页面或组件")
+  .option("-d,--dir <dir>", "在src下指定的目录。不指定时则在项目src/views目录下创建页面或src/components目录下创建组件，或者执行命令目录路径下创建页面或组件。")
   .action((type,name,options) => {
     if(type=='page'){
-      createPage(name,options.dir);
+      generate('page',name,options.dir);
     }else if(type.indexOf('com')>-1){
-      createComponent(name,options.dir);
+      generate('component',name,options.dir);
     }
   });
 /* = main entrance
